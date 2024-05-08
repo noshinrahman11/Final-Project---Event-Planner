f8f0f1eba5e5481c53d53696779600e4460d3b72
Noshin <noshinrahman11@gmail.com>
2024-05-08 14:01:58 -0400

    Added back buttons

--- static/APIRequest.js
+++ static/APIRequest.js
@@ -1,13 +1,13 @@
-// APIRequest.js
+// // APIRequest.js
 
-// Function to make an API request
-function apiRequest(path, obj, callback) {
-    axios.post(path, obj)
-        .then((response) => {
-            let result = response.data;
-            callback(result); // Call the callback function with the fetched data
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-}
+// // Function to make an API request
+// function apiRequest(path, obj, callback) {
+//     axios.post(path, obj)
+//         .then((response) => {
+//             let result = response.data;
+//             callback(result); // Call the callback function with the fetched data
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         });
+// }
